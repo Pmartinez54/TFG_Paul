b3dc53d14350898be44bf05ca136f991f36f5010
Pmartinez54 <pmartinezarmendariz54@gmail.com>
2024-10-30 12:12:30 +0000

    Barometroa1.0

---
+++ 1_Fasea/Barometro_irakurketa.c
@@ -0,0 +1,24 @@
+include<time.h>
+include<math.h>
+include<stdio.h>
+
+//Barometroak bueltatzen duen temperatura balioa gordetzen duen fitxategiaren helbidea.
+static const char TEMP_FITX[]="/sys/class/i2c-dev/i2c-1/device/1-0060/iio\:device0/in_temp_raw";
+//Barometroren  temperatura balioa gradu zentigradutan itzultzeko beharrezko biderkatzailea.
+static const float = 0.062500;
+//Barometroak bueltatzen duen presioaren balioa gordetzen duen fitxategiaren helbidea.
+static const char PRESIO_FITX[]="/sys/class/i2c-dev/i2c-1/device/1-0060/iio\:device0/in_temp_raw";
+//Barometroren  temperatura balioa gradu zentigradutan
+static const float = 0.062500;
+
+
+void main(){
+
+
+}
+
+void Temp_irakurketa(){
+
+
+
+}
---
+++ 1_Fasea/Sentsore_Irakurketa.c
@@ -0,0 +1,2 @@
+include <stid.h>
+
