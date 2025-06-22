ce37551cca7ae949ab75ef49ad813516247bdff2
Pmartinez54 <pmartinezarmendariz54@gmail.com>
2025-06-22 15:01:31 +0200

    Azkeneko_commit-a

--- 2_Fasea/scriptak/Irakurketak_exekutatu.sh
+++ 2_Fasea/scriptak/Irakurketak_exekutatu.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-##Konprobatzen du ea Senstsoreen irakurketa errutina exekutatzen ari den.
+##Konprobatzen du ea Senstsoreen irakurketa errutina exekutatzen ari den.. /root/.profile
 if  [ $(pidof Sentsoreen_irakurketa | wc -l) -eq 0 ]  
 then
+. /root/.profile
 ##Exekutatzen du senstore irakurketaren errutina.
 /etc/Sentsoreen_irakurketa/Sentsoreen_irakurketa
 fi
